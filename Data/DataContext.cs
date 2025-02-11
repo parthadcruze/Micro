4e55d67bd444584f72eedcc6abce0c65d17ec47f
Partha D Cruze <parthadcruze@gmail.com>
2025-02-11 19:41:28 +0530

    Initial commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microbiology.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Microbiology.Data
+{
+    public class DataContext(DbContextOptions options) : DbContext(options)
+    {
+    
+        public DbSet<Users> Login { get; set; }      
+        
+        
+    }
+}
---
+++ Entities/users.cs
@@ -0,0 +1,9 @@
+﻿namespace Microbiology.Entities
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+    }
+}
